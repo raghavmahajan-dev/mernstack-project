1968540e42040b2c1a1768e3511baeafdddbfbd6
raghavmahajan-dev <raghavmahajan1919@gmail.com>
2025-07-23 17:48:10 +0530

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+// middleware
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
+app.use(express.json());
+
+app.use("/api/notes", notesRoutes);
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
+});
